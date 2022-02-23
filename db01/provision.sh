6559c535bac0e00cc6f076dc1dceefa4bfef522f
Danilo Petruce Marques <danilopetruce@hotmail.com>
2022-02-23 04:04:22 -0300

    Upload de arquivos do Lab ao GitHub

---
+++ db01/provision.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cat << EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjkrffLuINw25T3nMqP0V9R6168I9s6RBdMrOdAk3IELgsnqBM6MVyBYjafBbZsOdQ3PFVc2U1U4Ygd8w9SxsweTqe8/gqeyDIwneQrev/dXNt68O7trmeOp2MAZWQ0D168yYni/6uWaxfChBZp33Pry3xzHNvDaP+dI2wzO+wMW7vpEv249rmScz1rrvflFFKp5ZkS35KBY6u9c5TrWXoM7ql+Ta/A83sIHABLoQYw+j04XGUjaNS9GNtEmwgbwf+0p0MpM1wPRzMb9ZSEwBs47huxolAxU8CKcrUMeJT3xTJ2pWRghy9RsJrXHmCEPhV1LF4E41yBAsh/l8AHnEL vagrant@control-node
+EOT
